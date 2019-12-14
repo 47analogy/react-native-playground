1b27986db18cc9e9ead2acdc0478d3d5ced50877
analogy47 <actweed47@gmail.com>
2019-12-14 01:41:33 -0500

    add todo and render todo list

--- sample-app-2/App.js
+++ sample-app-2/App.js
@@ -1,14 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
+  const [newTodo, setNewTodo] = useState("");
+  const [todoList, setTodoList] = useState([]);
+
+  const addTodoInputHandler = todoInput => {
+    setNewTodo(todoInput);
+  };
+
+  const addNewTodoHandler = () => {
+    setTodoList(currentTodos => [...currentTodos, newTodo]); // makes sure getting most current state
+  };
+
+  let todoIndex = 0;
+
   return (
     <View style={styles.screen}>
       <View style={styles.inputBox}>
-        <TextInput placeholder="Enter a todo" style={styles.input} />
-        <Button title="Add Todo" />
+        <TextInput
+          placeholder="Enter a todo"
+          style={styles.input}
+          onChangeText={addTodoInputHandler}
+          value={newTodo}
+        />
+        <Button title="Add" onPress={addNewTodoHandler} />
       </View>
-      <View></View>
+      <View>
+        {todoList.map(todo => (
+          <View style={styles.todoText} key={todoIndex++}>
+            <Text>{todo}</Text>
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
@@ -21,11 +45,19 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "space-between",
     alignItems: "center",
+    marginBottom: 20,
   },
   input: {
     width: 200,
-    borderColor: "black",
+    borderColor: "#000000",
     borderWidth: 1,
     padding: 10,
+  },
+  todoText: {
+    padding: 10,
+    borderColor: "#000000",
+    borderWidth: 1,
+    marginVertical: 10,
+    backgroundColor: "#add8e6",
   },
 });
